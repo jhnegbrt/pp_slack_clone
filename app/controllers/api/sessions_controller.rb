e66824923e0e88c095f37ae95e5d58f8d80dfcb6
jhnegbrt <jhnegbrt@gmail.com>
2021-03-31 18:14:56 -0700

    finishes sessions_controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,27 @@
 class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(user_params)
+    if @user.nil?
+      render json: ["Invalid Login Details!"], status: 404
+    else
+      login!(@user)
+      render 'api/users/show'
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      render :json {}
+    else
+      render :json ["No one is logged in!"], status: 404
+    end
+
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
