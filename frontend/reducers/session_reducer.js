71e2d4e8bd6e06c3ae4f3bea1b6340f618d0621c
jhnegbrt <jhnegbrt@gmail.com>
2021-03-31 23:41:29 -0700

    adds users_reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,5 +1,4 @@
-import RECEIVE_CURRENT_USER from '../actions/session_actions'
-import LOGOUT_CURRENT_USER from '../actions/session_actions'
+import {RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER} from '../actions/session_actions'
 
 const _nullUser = Object.freeze({
   id: null
@@ -8,7 +7,7 @@ const _nullUser = Object.freeze({
 const sessionReducer = (state = _nullUser, action) => {
   Object.freeze(state);
   switch(action.type){
-    case RECEIVER_CURRENT_USER:
+    case RECEIVE_CURRENT_USER:
       return Object.assign({}, {id: action.user.id})
     case LOGOUT_CURRENT_USER:
       return _nullUser;
