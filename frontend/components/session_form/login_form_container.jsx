25f345b5039ccecdb39c810f9b127e6ff98b1532
jhnegbrt <jhnegbrt@gmail.com>
2021-04-01 06:39:29 -0700

    adds login_form_container

---
+++ frontend/components/session_form/login_form_container.jsx
@@ -0,0 +1,17 @@
+import { connect } from  'react-redux'
+import SessionForm from './session_form'
+import { login } from '../../actions/session_actions'
+
+const mSTP = (state) => ({
+  errors: state.errors.session,
+  formType: "signin"
+})
+
+const mDTP = (dispatch) => ({
+  processForm: (user) => dispatch(login(user))  
+})
+
+export default connect(
+  mSTP,
+  mDTP
+)(SessionForm);
