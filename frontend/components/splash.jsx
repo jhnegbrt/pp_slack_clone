e47b1931a3533464c89bf3133f6583e506fdd431
jhnegbrt <jhnegbrt@gmail.com>
2021-04-03 06:12:45 -0700

    adds scrolling functionality to splash page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-scroll'
 
 
 function Header(){
@@ -6,8 +7,20 @@ function Header(){
     <div>
       <ul className="splash-header-list">
         <li>Sleuth</li>
-        <li>Introduction</li>
-        <li>Features</li>
+        
+        <li><a href="#splash-intro">Introduction</a></li>
+        {/* <li><a href="#features">Features</a></li> */}
+        <li><Link 
+          href="#features" 
+          to="features"
+          smooth={true}
+          duration={500}
+          spy={true}
+          offset={-200}
+          >
+            Features
+            </Link>
+        </li>
         <li>Technologies</li>
         <li>Sign Up</li>
         <li>Sign In/Demo</li>
@@ -22,14 +35,43 @@ function MainContainer(){
     <div className="splash-intro-left">
       <h1>Sleuth: A chat-app featuring anonymous 'Sleuth' mode</h1>
       <h2>Connect with your friends, family and co-workers through your personal
-        account, or as an anonymous <i>Sleuth</i>
+        account, or as an anonymous <i>Sleuth</i> (Learn More)
       </h2>
     </div>
     <div className="splash-intro-right">
       <div className="splash-spy"></div>
-     
     </div>
   </div>
+  )
+}
+
+function IntroductionContainer(){
+  return(
+    <div className="introduction-container">
+      <h1>take up space</h1>
+      <h1>take up space</h1>
+      <h1>take up space</h1>
+      <h1>take up space</h1>
+      <h1>take up space</h1>
+      <h1>take up space</h1>
+      <h1>take up space</h1>
+      <h1>take up space</h1>
+      <h1>take up space</h1>
+      <h1>take up space</h1>
+      <h1>take up space</h1>
+      <h1>take up space</h1>
+      <h1>take up space</h1>
+      <h1>take up space</h1>
+      <h1>take up space</h1>
+    </div>
+  )
+}
+
+function FeaturesContainer(){
+  return(
+    <div id="features">
+      <h1>Features</h1>
+    </div>
   )
 }
 
@@ -51,6 +93,10 @@ const Splash = () =>{
     <div className="splash-page">
       <Header></Header>
       <MainContainer></MainContainer>
+      <IntroductionContainer></IntroductionContainer>
+      <FeaturesContainer></FeaturesContainer>
+      <IntroductionContainer></IntroductionContainer>
+      <IntroductionContainer></IntroductionContainer>
       <Footer></Footer>
     </div>
   )
