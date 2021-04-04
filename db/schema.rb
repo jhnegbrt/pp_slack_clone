45065444695e946a1f666e1077c381b8a35a6861
jhnegbrt <jhnegbrt@gmail.com>
2021-04-04 03:02:18 -0700

    removes index on username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_225109) do
+ActiveRecord::Schema.define(version: 2021_04_04_100010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2021_03_31_225109) do
     t.string "session_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["username"], name: "index_users_on_username"
   end
 
 end
