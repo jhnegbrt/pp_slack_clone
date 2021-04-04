e672bae8053e62374742a5b7553893687ff5d536
jhnegbrt <jhnegbrt@gmail.com>
2021-04-04 02:58:32 -0700

    adds model level validation for username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   validates :password_digest, presence: true
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :session_token, presence: true
   validates :password, length: {minimum: 6}, allow_nil: true
 
