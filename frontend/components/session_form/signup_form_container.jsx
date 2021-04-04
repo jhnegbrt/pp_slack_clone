5398526e398b2b0ed39079c6333a8a67b286c5bb
jhnegbrt <jhnegbrt@gmail.com>
2021-04-03 21:05:28 -0700

    adds fixed splash navbar

--- frontend/components/session_form/signup_form_container.jsx
+++ frontend/components/session_form/signup_form_container.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {signup} from '../../actions/session_actions'
 import {connect} from 'react-redux'
 import SessionForm from './session_form'
+import { Link } from 'react-router-dom'
+
 
 const mSTP = (state) => ({
   errors: state.errors.session,
@@ -13,7 +15,20 @@ const mDTP = (dispatch) => ({
   processForm: (user) => dispatch(signup(user))
 })
 
+class SignupForm extends React.Component{
+  render(){
+    return(
+      <div>
+        <SessionForm></SessionForm>
+        <p>Already have an account? Log-In</p><Link to="/login">Here</Link>
+
+      </div>
+
+    )
+  }
+}
+
 export default connect(
   mSTP,
   mDTP
-)(SessionForm)
+)(SignupForm)
