2e870b3c48b2d784608d6d0509b5c17d7034060a
jhnegbrt <jhnegbrt@gmail.com>
2021-04-08 12:47:58 -0700

    adds regular RECEIVE_MESSAGES action creator and RECEIVE_MESSAGES constant

--- frontend/actions/message_actions.js
+++ frontend/actions/message_actions.js
@@ -1,5 +1,12 @@
 import * as APIUtil from '../util/message_api_util'
 
+export const RECEIVE_MESSAGE = "RECEIVE_MESSAGE"
+
+const receiveMessage  = (message) => ({
+  type: "RECEIVE_MESSAGE",
+  message
+})
+
 export const createMessage = formMessage => dispatch => {
   return(
     APIUtil.createMessage(formMessage)
