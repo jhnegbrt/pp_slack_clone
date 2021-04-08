c859954f04f1d3b9b42c2887d1b5af4baab961b7
jhnegbrt <jhnegbrt@gmail.com>
2021-04-08 11:14:02 -0700

    finishes messages controller

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,2 +1,46 @@
 class Api::MessagesController < ApplicationController
+
+  def index
+    @messages = Message.all
+  end
+
+  def show
+    @message = Message.find(params[:id])
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      render :show
+    else
+      render json: @message.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @message = Message.find(params[:id])
+
+    if @message.update(message_params)
+      render :show
+    else
+      render json: @message.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+
+    if @message.destroy
+      render :show
+    else
+      render json: @message.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:sender_id, :content, :parent_comment_id, :channel_dms_id)
+  end
 end
