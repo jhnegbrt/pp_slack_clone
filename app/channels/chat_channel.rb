ce57bc9ae6da62ac7f489b3822fc0391f8ce6ab7
jhnegbrt <jhnegbrt@gmail.com>
2021-04-11 05:36:29 -0700

    adds conditional logic to chat_channel.rb

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -6,12 +6,22 @@ class ChatChannel < ApplicationCable::Channel
 
   def speak(data)
     # debugger
-    message = Message.create(content: data['message']['content'], sender_id: data['message']['sender_id'])
-    socket = { 
-      id: message.id, 
-      content: message.content,
-      sender_id: message.sender_id,
-    }
+    message = Message.find_by(id: data['message']['id'])
+    if message.nil?
+      new_message = Message.create(content: data['message']['content'], sender_id: data['message']['sender_id'])
+      socket = { 
+        id: new_message.id, 
+        content: new_message.content,
+        sender_id: new_message.sender_id,
+      }
+    else
+      updated_message = message.update(content: data['message']['content'])
+      socket = { 
+        id: message.id, 
+        content: message.content,
+        sender_id: message.sender_id,
+      }
+    end
     ChatChannel.broadcast_to('chat_channel', socket)
   end
   def unsubscribed; end
