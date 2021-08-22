a4d36e068d2119a9a2110471314cda6390284000
jhnegbrt <jhnegbrt@gmail.com>
2021-08-22 15:09:34 -0700

    Adds search-wrapper div and underline beneath search

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -11474,6 +11474,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (function () {
@@ -11487,15 +11499,46 @@ __webpack_require__.r(__webpack_exports__);
     return state.workspace.threads;
   });
 
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(false),
+      _useState2 = _slicedToArray(_useState, 2),
+      displaySearch = _useState2[0],
+      setDisplaySearch = _useState2[1];
+
+  var _useState3 = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(""),
+      _useState4 = _slicedToArray(_useState3, 2),
+      searchEntry = _useState4[0],
+      setSearchEntry = _useState4[1];
+
+  function onClick(e) {
+    setDisplaySearch(true);
+  }
+
   function onSubmit(e) {
     e.preventDefault();
   }
 
+  function handleChange(e) {
+    setSearchEntry(e.target.value);
+  }
+
+  var search = /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "client-search"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "search-wrapper"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("form", {
+    onSubmit: onSubmit
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("input", {
+    type: "text",
+    placeholder: "Start typing to search!",
+    value: searchEntry,
+    onChange: handleChange
+  }))));
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     className: "search-bar"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-    className: "search-bar-button"
-  }, "Search for Direct Messages, Public Channels, or Joined Channels"));
+    className: "search-bar-button",
+    onClick: onClick
+  }, "Search for Direct Messages, Public Channels, or Joined Channels"), displaySearch ? search : "");
 });
 
 /***/ }),
--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import { useSelector } from "react-redux"
 
 export default () => {
@@ -6,14 +6,38 @@ export default () => {
   const publicChannels = useSelector(state => state.workspace.publicChannels)
   const users = useSelector(state => state.workspace.users)
   const threads = useSelector(state => state.workspace.threads)
+  const [displaySearch, setDisplaySearch] = useState(false)
+  const [searchEntry, setSearchEntry ] = useState("")
+
+  function onClick(e){
+    setDisplaySearch(true)
+  }
 
   function onSubmit(e){
     e.preventDefault()
   }
 
+  function handleChange(e){
+    setSearchEntry(e.target.value)
+  }
+
+  let search = (
+    <div className="client-search">
+      <div className="search-wrapper">
+        <form onSubmit={onSubmit}>
+          <input type="text" placeholder="Start typing to search!" value={searchEntry} onChange={handleChange}></input>
+        </form>
+      </div>
+    </div>
+  )
+
   return(
     <div className="search-bar">
-      <div className="search-bar-button">Search for Direct Messages, Public Channels, or Joined Channels</div>
+      <div 
+        className="search-bar-button" 
+        onClick={onClick}>Search for Direct Messages, Public Channels, or Joined Channels
+      </div>
+      {displaySearch ? search : ""}
     </div>
   )
 }
