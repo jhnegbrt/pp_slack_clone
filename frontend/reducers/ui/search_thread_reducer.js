49155f61e247d661c31b0e5c373cc1082c6a4334
jhnegbrt <jhnegbrt@gmail.com>
2021-08-28 23:42:41 -0700

    Adds searchThread reducer

--- frontend/reducers/ui/search_thread_reducer.js
+++ frontend/reducers/ui/search_thread_reducer.js
@@ -1,12 +1,11 @@
 import { RECEIVE_SEARCH_THREAD, CLEAR_SEARCH_THREAD } from "../../actions/search_thread_actions";
 
 const searchThreadReducer = (state = {}, action) => {
-
-  Object.freeze(state)
   switch(action.type){
     case RECEIVE_SEARCH_THREAD:
+      return {id: action.id}
     case CLEAR_SEARCH_THREAD:
-
+      return {}
     default:
       return state
   }
